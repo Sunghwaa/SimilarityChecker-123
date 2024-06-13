d4a30ccba705450460bfb703db4271f6d2648425
Sunghwaa <sunghwaa.lee@gmail.com>
2024-06-13 15:54:07 +0900

    [feature] 길이가 같을 때 TC 추가

--- length_checker.py
+++ length_checker.py
@@ -1,4 +1,7 @@
 class LengthChecker:
     def check(self, str1, str2):
+        if len(str1) == len(str2):
+            return 60
+
         if len(str1) >= len(str2) * 2 or len(str1) * 2 <= len(str2):
             return 0
--- test_length_checker.py
+++ test_length_checker.py
@@ -7,3 +7,7 @@ class TestLengthChecker(TestCase):
     def test_length_score_when_length_is_twice(self):
         self.checker = LengthChecker()
         self.assertEqual(0, self.checker.check("A", "BB"))
+
+    def test_length_score_when_length_is_same(self):
+        self.checker = LengthChecker()
+        self.assertEqual(60, self.checker.check("ASD", "DSA"))
