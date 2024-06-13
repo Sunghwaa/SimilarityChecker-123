d5f43fce7a1c412db7761a016188165cdb64bc6b
Sunghwaa <sunghwaa.lee@gmail.com>
2024-06-13 16:02:02 +0900

    [refactoring] 중복 코드 제거

--- test_length_checker.py
+++ test_length_checker.py
@@ -4,10 +4,12 @@ from length_checker import LengthChecker
 
 
 class TestLengthChecker(TestCase):
-    def test_length_score_when_length_is_twice(self):
+    def setUp(self):
+        super().setUp()
         self.checker = LengthChecker()
+
+    def test_length_score_when_length_is_twice(self):
         self.assertEqual(0, self.checker.check("A", "BB"))
 
     def test_length_score_when_length_is_same(self):
-        self.checker = LengthChecker()
         self.assertEqual(60, self.checker.check("ASD", "DSA"))
