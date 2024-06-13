f4191b175e14d374a545b5984facca8073df62ab
Sunghwaa <sunghwaa.lee@gmail.com>
2024-06-13 16:13:18 +0900

    [refactoring] extract method로 가독성 향상 및 level 맞추기

--- length_checker.py
+++ length_checker.py
@@ -1,9 +1,18 @@
 class LengthChecker:
     def check(self, str1, str2):
-        if len(str1) == len(str2):
+        if self.is_length_same(str1, str2):
             return 60
 
-        if len(str1) >= len(str2) * 2 or len(str1) * 2 <= len(str2):
+        if self.is_length_diff_more_than_twice(str1, str2):
             return 0
 
+        return self.calc_score(str1, str2)
+
+    def is_length_same(self, str1, str2):
+        return len(str1) == len(str2)
+
+    def is_length_diff_more_than_twice(self, str1, str2):
+        return len(str1) >= len(str2) * 2 or len(str1) * 2 <= len(str2)
+
+    def calc_score(self, str1, str2):
         return int((1 - (len(str1) - len(str2)) / len(str2)) * 60)
