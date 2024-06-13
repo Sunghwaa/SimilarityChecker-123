08e4e54b6cc772298ba13ff77f0bc824a7a54ed4
Sunghwaa <sunghwaa.lee@gmail.com>
2024-06-13 15:51:38 +0900

    [feature] 길이 차이가 2배 이상일 때 TC 추가

--- length_checker.py
+++ length_checker.py
@@ -1,2 +1,4 @@
 class LengthChecker:
-    pass
+    def check(self, str1, str2):
+        if len(str1) >= len(str2) * 2 or len(str1) * 2 <= len(str2):
+            return 0
--- test_length_checker.py
+++ test_length_checker.py
@@ -1,5 +1,9 @@
 from unittest import TestCase
 
+from length_checker import LengthChecker
+
 
 class TestLengthChecker(TestCase):
-    pass
+    def test_length_score_when_length_is_twice(self):
+        self.checker = LengthChecker()
+        self.assertEqual(0, self.checker.check("A", "BB"))
